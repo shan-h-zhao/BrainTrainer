2b7d3e87c973805fcf5651ea65ca7f37f339791e
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-21 08:33:51 -0500

    Fix the repeated random answer issue

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -32,54 +32,57 @@ public class MainActivity extends AppCompatActivity {
     int countCorrect = 0;
     int countTotal = 0;
 
+    CountDownTimer timer;
+
+    Boolean gameOver = false;
+
 
     // Create a function to generate a question and answer buttons
     public void updateQuestion () {
-        // Generate a "plus" question
-        final int random1 = new Random().nextInt(10);
-        final int random2 = new Random().nextInt(10);
-        questionTextView.setText(Integer.toString(random1) + "+" + Integer.toString(random2));
 
-        // Calculate the correct result
-        final int result0 = random1 + random2;
+        if (!gameOver) {
+            // Generate a "plus" question
+            final int random1 = new Random().nextInt(10);
+            final int random2 = new Random().nextInt(10);
+            questionTextView.setText(Integer.toString(random1) + "+" + Integer.toString(random2));
 
-        // Set textView for the four buttons
-        List<Button> buttons = new ArrayList<Button>();
-        buttons.add(button0);
-        buttons.add(button1);
-        buttons.add(button2);
-        buttons.add(button3);
+            // Calculate the correct result
+            final int correctResult = random1 + random2;
 
-        correctTag = new Random().nextInt(4);
-        for (Button b: buttons) {
+            // Set textView for the four buttons
+            List<Button> buttons = new ArrayList<Button>();
+            buttons.add(button0);
+            buttons.add(button1);
+            buttons.add(button2);
+            buttons.add(button3);
 
-            Log.i("correctTag", String.valueOf(correctTag));
-            Log.i("getTag", String.valueOf(b.getTag()));
-            Log.i("getTag", String.valueOf(b.getTag().equals(Integer.toString(correctTag))));
-
-            if (b.getTag().equals(Integer.toString(correctTag))) {
-                b.setText(Integer.toString(result0));
-            } else {
-                b.setText(Integer.toString(new Random().nextInt(20)));
+            correctTag = new Random().nextInt(4);
+            for (Button b: buttons) {
+                if (b.getTag().equals(Integer.toString(correctTag))) {
+                    b.setText(Integer.toString(correctResult));
+                } else {
+                    int randomAnswer = new Random().nextInt(20);
+                    while (randomAnswer == correctResult) {
+                        randomAnswer = new Random().nextInt(20);
+                    }
+                    b.setText(Integer.toString(randomAnswer));
+                }
             }
         }
-
     }
 
     // onClick function to check if the correct answer is clicked, and update the question
     public void pickAnswer(View view) {
         Button clicked = (Button) view;
-        Log.i("clicked button", String.valueOf(clicked.getTag()));
-        Log.i("correcttag", String.valueOf(correctTag));
-        if (clicked.getTag().equals(Integer.toString(correctTag))) {
-            countCorrect += 1;
-            Log.i("clicked correct button", Integer.toString(correctTag));
-        } else {
-            Log.i("incorrect button", String.valueOf(clicked.getTag()));
+
+        if (!gameOver) {
+            if (clicked.getTag().equals(Integer.toString(correctTag))) {
+                countCorrect += 1;
+            }
+            countTotal += 1;
+            scoreTextView.setText(String.valueOf(countCorrect) + "/" + String.valueOf(countTotal));
+            updateQuestion ();
         }
-        countTotal += 1;
-        scoreTextView.setText(String.valueOf(countCorrect) + "/" + String.valueOf(countTotal));
-        updateQuestion ();
     }
 
     // Create a function to convert seconds to hh:mm:ss
@@ -89,6 +92,16 @@ public class MainActivity extends AppCompatActivity {
         return df.format(milliSeconds);
     }
 
+    // onClick function to restart the game
+    public void playAgain(View view) {
+        gameOver = false;
+        timer.start();
+        updateQuestion();
+        countTotal = 0;
+        countCorrect = 0;
+        scoreTextView.setText(String.valueOf(countCorrect) + "/" + String.valueOf(countTotal));
+
+    }
 
 
     @Override
@@ -97,12 +110,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         questionTextView = findViewById(R.id.questionTextView);
+
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
 
         TextView timerTextView = findViewById(R.id.timerTextView);
+
+        TextView gameOverTextView = findViewById(R.id.gameOverTextView);
+        gameOverTextView.setVisibility(View.INVISIBLE);
+
+        Button playAgainButton = findViewById(R.id.playAgainButton);
+        playAgainButton.setVisibility(View.INVISIBLE);
 
         // Update question
         updateQuestion ();
@@ -112,18 +132,27 @@ public class MainActivity extends AppCompatActivity {
         scoreTextView.setText("0/0");
 
         // Countdown timer
-        new CountDownTimer(60000, 1000) {
+        timer = new CountDownTimer(30000, 1000) {
             @Override
             public void onTick(long l) {
                 String timeLeft = convertTime (new Date(l));
                 timerTextView.setText(timeLeft);
+                gameOverTextView.setVisibility(View.INVISIBLE);
+                playAgainButton.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFinish() {
+                gameOverTextView.setVisibility(View.VISIBLE);
+                playAgainButton.setVisibility(View.VISIBLE);
+                gameOver = true;
 
             }
-        }.start();
+        };
+
+        timer.start();
+
+
 
 
 
